b88c3bbdbcf8711c70df7599a5fbfcefd6d1cd09
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 14:33:44 +0500

    Initial Commit

---
+++ PushNotifications/PushNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsApp.swift
+//  PushNotifications
+//
+//  Created by Elexoft on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
