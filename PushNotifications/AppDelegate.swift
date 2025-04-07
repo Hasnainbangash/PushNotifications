d7784783aff8294ee3059e74420822b292df57bb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 15:21:18 +0500

    Added the two new delegate protocols names UNUserNotificationCenterDelegate, MessagingDelegate and alogn with the added the method didRegisterForRemoteNotificationsWithDeviceToken

--- PushNotifications/AppDelegate.swift
+++ PushNotifications/AppDelegate.swift
@@ -10,11 +10,15 @@ import FirebaseCore
 import FirebaseMessaging
 import UserNotifications
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
     }
     
 }
