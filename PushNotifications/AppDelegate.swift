68bf894e08fdd29162449c0c3ffbcc3916f98bff
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 15:17:21 +0500

    Firebase core firebsase messa=sing and the user notoifcations ia added to the ptoject along with the firebase app s confure in the dusFinishlaunching wiuthoptions funstion nfor te firebase

--- PushNotifications/AppDelegate.swift
+++ PushNotifications/AppDelegate.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
         return true
     }
     
