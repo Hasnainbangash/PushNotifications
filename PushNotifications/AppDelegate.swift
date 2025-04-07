e0dcb77f58912d717bca99cc5648b140cdc03ebb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 14:37:52 +0500

    AppDelegate file is added to the project added the function os didFInishLaunch9ng WIth options

---
+++ PushNotifications/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  PushNotifications
+//
+//  Created by Elexoft on 07/04/2025.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+}
