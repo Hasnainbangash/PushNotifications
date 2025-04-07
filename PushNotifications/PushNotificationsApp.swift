a99c43ccd4f291fd43a6d394caae02672931b0d5
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-04-07 14:40:20 +0500

    Giving the reference to the app delegate work dne along wit the comments area deed to the project

--- PushNotifications/PushNotificationsApp.swift
+++ PushNotifications/PushNotificationsApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct PushNotificationsApp: App {
+    // Giving the reference to the App delegate file
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
